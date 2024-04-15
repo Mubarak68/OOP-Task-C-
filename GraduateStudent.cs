73113bf5983e291962844d7738acf89ed55e7ea8
KFH <KFH@LAPTOP-83314>
2024-04-15 11:24:00 +0300

    Add project files.

---
+++ GraduateStudent.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//OOP
+
+//Task 1
+public class GraduateStudent : Student
+{
+    string TheisTopic { get; set; }
+    public GraduateStudent(string name, string major, double gpa,string theisTopic) 
+        : base(name, major, gpa)
+    {
+        TheisTopic = theisTopic;
+    }
+
+   public new void PrintSummary()
+    {
+        Console.WriteLine("Graduated Student:");
+        base.PrintStudentFormatted();
+        Console.WriteLine($"Reserch Topic: {TheisTopic}");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+//OOP
+
+//Task 1
+using System.Security.Cryptography;
+
+public class Student
+{
+    public string Name { get; set; }
+    public string Major { get; set; }
+    public double GPA { get; set; }
+
+    public Student(string name, string major, double gpa)
+    {
+        Name = name;
+        Major = major;
+        GPA = gpa;
+    }
+
+    //Task 2
+    public void PrintStudentFormatted()
+    {
+        Console.WriteLine("Student Information:");
+        Console.WriteLine($"Student name: \t{Name}\nStudent Major: \t{Major}\nStudent GPA: \t{GPA} ");
+        
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student student = new Student("Mubarak", "CE", 3.82);
+        student.PrintStudentFormatted();
+        Console.WriteLine("--------------------------------------");
+        GraduateStudent graduation = new GraduateStudent("Ahmad", "IST",3.30,"Physics");
+        graduation.PrintSummary();
+    }
+}
